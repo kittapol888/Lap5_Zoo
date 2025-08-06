7e1316d46b939883bc7779be925ba640436d9047
kittapol888 <kittapol.sa@ku.th>
2025-08-07 02:00:07 +0700

    Create Lion.java

---
+++ lib/Lion.java
@@ -0,0 +1,12 @@
+package Lib;
+
+public class Lion extends Mammal {
+
+    public Lion(String name) {
+        super(name);
+    }
+    @Override
+    public String makeSound() {
+        return "Roar!";
+    }
+}
