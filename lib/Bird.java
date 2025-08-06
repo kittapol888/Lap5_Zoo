0807a2bab2339851dac3e2a99dfa87a858fa929d
kittapol888 <kittapol.sa@ku.th>
2025-08-07 01:58:40 +0700

    Create Bird.java

---
+++ lib/Bird.java
@@ -0,0 +1,13 @@
+package Lib;
+
+public class Bird extends Animal {
+    public Bird(String name){
+        super(name);
+    }
+
+    @Override
+    public String makeSound() {
+        return "Bird chirp";
+    }
+    
+}
